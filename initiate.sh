7274706cff78bc0888a34c666731645aeff7c588
Yves Stadler <mvyonline@gmail.com>
2011-10-02 23:37:53 +0200

    Make images

--- initiate.sh
+++ initiate.sh
@@ -6,6 +6,7 @@ read m
 
 mkdir CH$n-$m
 cd CH$n-$m
+mkdir images
 
 sed -e "s/{NUMBER}/$n/" ../Template/Slides.tex > CH$n-Slides.tex
 sed -e "s/{NUMBER}/$n/" ../Template/Handout.tex > CH$n-Handout.tex
